81c854f56453926ce7e304cd0b17fb2019402d13
Atsushi Shibata <shibata@webcore.co.jp>
2017-12-21 16:50:02 +0900

    initial commit

---
+++ config.py
@@ -0,0 +1,14 @@
+# システムの設定値
+
+from bing_recognizer import Bing
+
+SAMPLE_RATE = 16000
+VOLUME_THRESHOLD = 200
+STARTUP_TIME = 3
+SILENCE_LIMIT = 1
+PREV_LENGTH = 0.5
+MAX_SECOND = 9.5
+
+RECOGNIZER = Bing
+BING_KEY = '(BING KEY)'
+
