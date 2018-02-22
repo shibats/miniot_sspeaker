fac09dd7431d4d064ab610de2b5ce7530d8c87cf
Atsushi Shibata <shibata@webcore.co.jp>
2018-02-22 14:50:46 +0900

    天気予報系の設定文字列を追加

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 from bing_recognizer import Bing
 
 SAMPLE_RATE = 16000
-VOLUME_THRESHOLD = 200
+VOLUME_THRESHOLD = 2000
 STARTUP_TIME = 3
 SILENCE_LIMIT = 1
 PREV_LENGTH = 0.5
@@ -12,5 +12,8 @@ MAX_SECOND = 9.5
 WAKE_WORD = 'ラズパイ'
 
 RECOGNIZER = Bing
-BING_KEY = '(Bing API Key)'
+BING_KEY = 'db0b1e5e283a47699408dfac3b617f83'
+
+WR_URL = 'https://tenki.jp/week/3/'
+WR_INDEX = 0
 
