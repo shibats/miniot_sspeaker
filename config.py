225e97a9c8aae9055859746a9eb4f5f4578a13aa
Atsushi Shibata <shibata@webcore.co.jp>
2018-02-23 15:16:17 +0900

    音関連の設定を追加，値を微調整

--- config.py
+++ config.py
@@ -3,17 +3,28 @@
 from bing_recognizer import Bing
 
 SAMPLE_RATE = 16000
-VOLUME_THRESHOLD = 2000
+VOLUME_THRESHOLD = 200
 STARTUP_TIME = 3
-SILENCE_LIMIT = 1
-PREV_LENGTH = 0.5
+SILENCE_LIMIT = 2
+PREV_LENGTH = 1.0
 MAX_SECOND = 9.5
 
 WAKE_WORD = 'ラズパイ'
 
 RECOGNIZER = Bing
-BING_KEY = '(Bing Speech APIのキー )'
+BING_KEY = '(Bing Speech APIのキー)'
+
+# 天気予報用のURLとインデックス
 
 WR_URL = 'https://tenki.jp/week/3/'
 WR_INDEX = 0
+
+# 効果音
+
+# 起動音
+STARTUP = './sounds/startup.wav'
+# ウェイクワード認識，コマンド待ち
+COMMANDREADY = './sounds/commandready.wav'
+# コマンド認識失敗
+FAILURE = './sounds/failure.wav'
 
