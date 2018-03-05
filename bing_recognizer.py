3480902811a862137e64d4be63827c7c40075692
Atsushi Shibata <shibata@webcore.co.jp>
2018-03-05 16:45:13 +0900

    コメントを日本語に差し替え。

--- bing_recognizer.py
+++ bing_recognizer.py
@@ -24,24 +24,20 @@ class Bing:
     def recognize(self, audio_data, config=None, key='',
                   language="ja-JP", show_all=False):
         """
-        Performs speech recognition on ``audio_data`` (an ``AudioData`` instance), using the Microsoft Bing Speech API.
-
-        The Microsoft Bing Speech API key is specified by ``key``. Unfortunately, these are not available without `signing up for an account <https://azure.microsoft.com/en-ca/pricing/details/cognitive-services/speech-api/>`__ with Microsoft Azure.
-
-        To get the API key, go to the `Microsoft Azure Portal Resources <https://portal.azure.com/>`__ page, go to "All Resources" > "Add" > "See All" > Search "Bing Speech API > "Create", and fill in the form to make a "Bing Speech API" resource. On the resulting page (which is also accessible from the "All Resources" page in the Azure Portal), go to the "Show Access Keys" page, which will have two API keys, either of which can be used for the `key` parameter. Microsoft Bing Speech API keys are 32-character lowercase hexadecimal strings.
-
-        The recognition language is determined by ``language``, a BCP-47 language tag like ``"en-US"`` (US English) or ``"fr-FR"`` (International French), defaulting to US English. A list of supported language values can be found in the `API documentation <https://docs.microsoft.com/en-us/azure/cognitive-services/speech/api-reference-rest/bingvoicerecognition#recognition-language>`__ under "Interactive and dictation mode".
-
-        Returns the most likely transcription if ``show_all`` is false (the default). Otherwise, returns the `raw API response <https://docs.microsoft.com/en-us/azure/cognitive-services/speech/api-reference-rest/bingvoicerecognition#sample-responses>`__ as a JSON dictionary.
-
-        Raises a ``speech_recognition.UnknownValueError`` exception if the speech is unintelligible. Raises a ``speech_recognition.RequestError`` exception if the speech recognition operation failed, if the key isn't valid, or if there is no internet connection.
+        Microsoft Bing Speech APIを使って音声認識を実行するメソッド。
+        audio_data(AudioData)に音声ファイル，
+        configに設定オブジェクト，keyにAPI Keyを渡して呼び出すと，
+        APIを呼び出して結果を返す。
+        show_allがTrueだとレスポンスのJSONを辞書に変換して返す。
+        Falseだと，認識した文字列を返す。
         """
 
+        # キャッシュしたaccess_tokenとexpire_timeを取り出す
         access_token, expire_time = getattr(self, "bing_cached_access_token", None), getattr(self, "bing_cached_access_token_expiry", None)
         allow_caching = True
         from time import monotonic
-        if expire_time is None or monotonic() > expire_time:  # caching not enabled, first credential request, or the access token from the previous one expired
-            # get an access token using OAuth
+        if expire_time is None or monotonic() > expire_time:
+            # キャッシュが無効の場合，access_tokenなどを取得する
             credential_url = "https://api.cognitive.microsoft.com/sts/v1.0/issueToken"
             credential_request = Request(credential_url, data=b"", headers={
                 "Content-type": "application/x-www-form-urlencoded",
@@ -53,7 +49,7 @@ class Bing:
                 start_time = monotonic()
 
             try:
-                credential_response = urlopen(credential_request, timeout=60)  # credential response can take longer, use longer timeout instead of default one
+                credential_response = urlopen(credential_request, timeout=60)
             except HTTPError as e:
                 raise RequestError("credential request failed: {}".format(e.reason))
             except URLError as e:
@@ -61,10 +57,11 @@ class Bing:
             access_token = credential_response.read().decode("utf-8")
 
             if allow_caching:
-                # save the token for the duration it is valid for
+                # access_tokenを保存する
                 self.bing_cached_access_token = access_token
                 self.bing_cached_access_token_expiry = start_time + 600  # according to https://docs.microsoft.com/en-us/azure/cognitive-services/speech/api-reference-rest/bingvoicerecognition, the token expires in exactly 10 minutes
 
+        # wavのデータを，APIがサポートした形式にコンバートする
         wav_data = audio_data.get_wav_data(
             convert_rate=16000,  # audio samples must be 8kHz or 16 kHz
             convert_width=2  # audio samples should be 16-bit
@@ -76,13 +73,16 @@ class Bing:
             "requestid": uuid.uuid4(),
         }))
 
-        if sys.version_info >= (3, 6):  # chunked-transfer requests are only supported in the standard library as of Python 3.6+, use it if possible
+        if sys.version_info >= (3, 6):
+            # Python 3.6以上の場合，
+            # chunked-transferリクエストを使いAPIを呼び出す
             request = Request(url, data=io.BytesIO(wav_data), headers={
                 "Authorization": "Bearer {}".format(access_token),
                 "Content-type": "audio/wav; codec=\"audio/pcm\"; samplerate=16000",
                 "Transfer-Encoding": "chunked",
             })
-        else:  # fall back on manually formatting the POST body as a chunked request
+        else:
+            # Python 3.6以下の場合，POSTでAPIを呼び出すする
             ascii_hex_data_length = "{:X}".format(len(wav_data)).encode("utf-8")
             chunked_transfer_encoding_data = ascii_hex_data_length + b"\r\n" + wav_data + b"\r\n0\r\n\r\n"
             request = Request(url, data=chunked_transfer_encoding_data, headers={
@@ -100,7 +100,7 @@ class Bing:
         response_text = response.read().decode("utf-8")
         result = json.loads(response_text)
 
-        # return results
+        # 結果を返す
         if show_all: return result
         if "RecognitionStatus" not in result or result["RecognitionStatus"] != "Success" or "DisplayText" not in result: raise UnknownValueError()
         print(result)
@@ -108,6 +108,9 @@ class Bing:
 
 
 def test_recognize(key, filename):
+    """
+    Microsoft Bing Speech APIを使って音声認識するテスト用関数
+    """
     from audio import AudioData, AudioFile
     af = AudioFile(filename)
     af.__enter__()
